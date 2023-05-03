f731ca2967dabe53a75c3f32931b350b866cc87d
themefju <98777676+themefju@users.noreply.github.com>
2023-05-03 11:02:00 +0000

    Add get access token function (#1)

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,6 +1,14 @@
 import { expect } from 'chai';
+import { getAccessToken } from '../utils/getAccessToken';
 
 it('works', function () {
   expect(true).to.be.true;
   expect(false).to.be.false;
+});
+
+it('generates token', async function () {
+  const { token_type, expires_in, access_token } = await getAccessToken();
+  expect(token_type).to.equal('Bearer');
+  expect(expires_in).to.equal(3600);
+  expect(access_token).to.be.a.string;
 });
---
+++ utils/getAccessToken.ts
@@ -0,0 +1,41 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { TokenObject } from './types';
+
+const BASE_URL = process.env.BASE_URL as string;
+const TOKEN_PATH = process.env.TOKEN_ENDPOINT as string;
+const TOKEN_URL = BASE_URL.concat(TOKEN_PATH);
+
+const GRANT_TYPE = process.env.GRANT_TYPE as string;
+const CLIENT_ID = process.env.CLIENT_ID as string;
+const CLIENT_SECRET = process.env.CLIENT_SECRET as string;
+
+const params = new URLSearchParams({
+  grant_type: GRANT_TYPE,
+  client_id: CLIENT_ID,
+  client_secret: CLIENT_SECRET,
+});
+
+export async function getAccessToken(): Promise<TokenObject> {
+  const response: TokenObject = await fetch(new URL(TOKEN_URL), {
+    method: 'POST',
+    body: params,
+  })
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      }
+
+      // dont throw json to catch block and
+      // dont throw new Error if wanna test e.g. 400 status response
+      return response.json().then(function (json: object) {
+        throw json;
+      });
+    })
+    .catch(function (error: object) {
+      throw new Error(JSON.stringify(error));
+    });
+
+  return response;
+}
