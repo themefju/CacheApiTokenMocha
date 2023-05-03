5039e29adc01b200984cc1cee3615d70b4aa14cd
themefju <98777676+themefju@users.noreply.github.com>
2023-05-03 12:02:21 +0000

    Add send get request function (#2)

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,5 +1,7 @@
 import { expect } from 'chai';
 import { getAccessToken } from '../utils/getAccessToken';
+import { sendGetRequest } from '../utils/sendRequest';
+import { AnimalsResponse } from '../utils/types';
 
 it('works', function () {
   expect(true).to.be.true;
@@ -8,7 +10,24 @@ it('works', function () {
 
 it('generates token', async function () {
   const { token_type, expires_in, access_token } = await getAccessToken();
+
   expect(token_type).to.equal('Bearer');
   expect(expires_in).to.equal(3600);
   expect(access_token).to.be.a.string;
+});
+
+describe('/animals endpoint', function () {
+  const PATH = '/animals';
+
+  it('works with query parameters', async function () {
+    const params = new URLSearchParams({
+      type: 'dog',
+      limit: '2',
+    }).toString();
+    const { animals } = (await sendGetRequest(
+      `${PATH}?${params}`
+    )) as AnimalsResponse;
+
+    expect(animals).to.have.length(2);
+  });
 });
---
+++ utils/sendRequest.ts
@@ -0,0 +1,33 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { getAccessToken } from './getAccessToken';
+
+const BASE_URL = process.env.BASE_URL as string;
+
+export async function sendGetRequest(path: string): Promise<object> {
+  const fullPath = BASE_URL.concat(path);
+  const { access_token } = await getAccessToken();
+  const response: object = await fetch(new URL(fullPath), {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  })
+    .then(function (response) {
+      if (response.ok) {
+        // dont return .json() if wanna test response status codes
+        // instead return response object and check codes in tests
+        return response.json();
+      }
+
+      return response.json().then(function (json: object) {
+        throw json;
+      });
+    })
+    .catch(function (error: object) {
+      throw new Error(JSON.stringify(error));
+    });
+
+  return response;
+}
